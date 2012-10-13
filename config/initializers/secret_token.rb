968ca47bf126f82036bcfa553ee62f9e2f6d1171
carl sednaoui <carlsednaoui@gmail.com>
2012-10-13 01:05:25 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GaProgrammingFundamentals::Application.config.secret_token = '90520e659f7f533378be4b47ad84dbb282e362b47695aeede7c465eecaa1e915fde4b423e3fdd85ef0567d2f5aa586a8ec7fddadee5789ea4a67933ccf9a0cf3'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+GaProgrammingFundamentals::Application.config.session_store :cookie_store, key: '_ga-programming-fundamentals_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# GaProgrammingFundamentals::Application.config.session_store :active_record_store
