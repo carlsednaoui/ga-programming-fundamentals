6845882f58394678efcc7121edf215518a2d2f89
carl sednaoui <carlsednaoui@gmail.com>
2012-10-13 14:29:26 -0400

    work on user layout, routes and controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
+
 class UsersController < ApplicationController
-  # GET /users
-  # GET /users.json
+before_filter :view_users_auth, :only => [:index, :destroy]
+
   def index
     @users = User.all
 
@@ -10,19 +11,6 @@ class UsersController < ApplicationController
     end
   end
 
-  # GET /users/1
-  # GET /users/1.json
-  def show
-    @user = User.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @user }
-    end
-  end
-
-  # GET /users/new
-  # GET /users/new.json
   def new
     @user = User.new
 
@@ -32,20 +20,15 @@ class UsersController < ApplicationController
     end
   end
 
-  # GET /users/1/edit
-  def edit
-    @user = User.find(params[:id])
-  end
-
-  # POST /users
-  # POST /users.json
   def create
     @user = User.new(params[:user])
 
     respond_to do |format|
       if @user.save
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
-        format.json { render json: @user, status: :created, location: @user }
+        format.html { redirect_to root_path, notice: 'User was successfully created.' }
+        format.json { render json: "great success", status: :created, location: @user }
+        # format.html { redirect_to @user, notice: 'User was successfully created.' }
+        # format.json { render json: @user, status: :created, location: @user }
       else
         format.html { render action: "new" }
         format.json { render json: @user.errors, status: :unprocessable_entity }
@@ -53,24 +36,6 @@ class UsersController < ApplicationController
     end
   end
 
-  # PUT /users/1
-  # PUT /users/1.json
-  def update
-    @user = User.find(params[:id])
-
-    respond_to do |format|
-      if @user.update_attributes(params[:user])
-        format.html { redirect_to @user, notice: 'User was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /users/1
-  # DELETE /users/1.json
   def destroy
     @user = User.find(params[:id])
     @user.destroy
@@ -80,4 +45,14 @@ class UsersController < ApplicationController
       format.json { head :no_content }
     end
   end
+
+  protected
+
+  # Basic HTML Authentication to view users
+  def view_users_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == "ga" && password == "ga"
+    end
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,20 @@
 GaProgrammingFundamentals::Application.routes.draw do
-  root to: "users#index"
+  root to: "users#new"
 
-  resources :users
+  get "/users/new" => "users#new", :as => "new_user"
+  post "/users" => "users#create", :as => "create_user"
+  get "/users" => "users#index", :as => "users"
+
+  delete "/users/:id" => "users#destroy", :as => "delete_user"
+
+  # resources :users
+  # this creates the below routes
+  #      root        /                         users#new
+  #     users GET    /users(.:format)          users#index
+  #           POST   /users(.:format)          users#create
+  #  new_user GET    /users/new(.:format)      users#new
+  # edit_user GET    /users/:id/edit(.:format) users#edit
+  #      user GET    /users/:id(.:format)      users#show
+  #           PUT    /users/:id(.:format)      users#update
+  #           DELETE /users/:id(.:format)      users#destroy
 end
